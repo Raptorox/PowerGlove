8e02a393b0751b3d9710ea31bc03c147b77a04fc
Raptorox <70806316+Raptorox@users.noreply.github.com>
2023-11-16 19:28:21 +0100

    Init

---
+++ src/client/kotlin/powerglove/PowerGloveClient.kt
@@ -0,0 +1,9 @@
+package powerglove
+
+import net.fabricmc.api.ClientModInitializer
+
+object PowerGloveClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
